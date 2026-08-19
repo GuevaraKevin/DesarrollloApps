f2ad00bcd04335c9893b1910400757e5223e512b
GuevaraKevin <GuevaraKevin@users.noreply.github.com>
2026-08-19 09:39:13 -0500

    Finalize move: update gitignore path

---
+++ .angular/cache/21.2.21/semana3/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,41 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-5VLLRJ53.js";
+import "./chunk-ZBQXT5RQ.js";
+import "./chunk-MH7D5KJE.js";
+import "./chunk-SNY23LV3.js";
+import "./chunk-GRYWTKPJ.js";
+import "./chunk-PVH3IZ4J.js";
+import "./chunk-S6STQDT5.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
